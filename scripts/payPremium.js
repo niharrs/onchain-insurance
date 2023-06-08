dd2504749f429e06e7f12eb5aef6e89cf859f546
Niharika Singh <niharika.3297@gmail.com>
2023-06-08 12:16:03 +0200

    Add payPremium script

---
+++ scripts/payPremium.js
@@ -0,0 +1,22 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  // Retrieve the contract instance using the contract address
+  const contractAddress = "0x0048858826f97b0908793F914630b1739D5f3422";
+  const Insurance = await ethers.getContractFactory("Insurance");
+  const insurance = await Insurance.attach(contractAddress);
+
+  // Call contract functions
+  const payPremiumTxResponse = await insurance.payPremium({
+    value: ethers.utils.parseEther("0.1"),
+  });
+  await payPremiumTxResponse.wait(1);
+  console.log("Premium paid!");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
