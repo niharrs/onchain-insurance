b0abcb9a9abb923b9a182bc22bc87e33d90d97e5
Niharika Singh <niharika.3297@gmail.com>
2023-06-08 13:20:33 +0200

    Add getClaimsByAccount script

---
+++ scripts/getClaimsByAccount.js
@@ -0,0 +1,24 @@
+const { BigNumber } = require("ethers");
+const { ethers } = require("hardhat");
+
+async function main() {
+  // Retrieve the contract instance using the contract address
+  const contractAddress = "0x0048858826f97b0908793F914630b1739D5f3422";
+  const Insurance = await ethers.getContractFactory("Insurance");
+  const insurance = await Insurance.attach(contractAddress);
+
+  const address = "0x545e3FCFcf6E34C73F881E92eBD1Dd30D5CfB8cA"; //Replace with UI
+
+  // Call contract functions
+  const getClaimsByAccountTxResponse = await insurance.getClaimsByAccount(
+    address
+  );
+  console.log(getClaimsByAccountTxResponse);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
