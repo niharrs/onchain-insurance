066de5a64e0e1a739e9c55a89f29e0b4d8d4332f
Niharika Singh <niharika.3297@gmail.com>
2023-06-08 11:59:39 +0200

    Add wowen configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const WOWEN_RPC_URL = process.env.WOWEN_RPC_URL;
+const WOWEN_PRIV_KEY = process.env.WOWEN_PRIV_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
+  networks: {
+    wowen: {
+      url: WOWEN_RPC_URL,
+      accounts: [WOWEN_PRIV_KEY],
+      chainId: 981,
+    },
+  },
   solidity: "0.8.18",
 };
